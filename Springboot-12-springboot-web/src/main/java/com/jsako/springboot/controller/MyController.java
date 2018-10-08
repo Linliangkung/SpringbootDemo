45cab55d36c78e98daeb1a0617545b8bc59d611f
linliangkung <359270069@qq.com>
2018-10-08 18:06:44 +0800

    提交测试idea与eclipse路径问题代码

--- Springboot-12-springboot-web/src/main/java/com/jsako/springboot/controller/MyController.java
+++ Springboot-12-springboot-web/src/main/java/com/jsako/springboot/controller/MyController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Date 2018/9/20
  * @Author LLJ
@@ -14,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @GetMapping("/helloworld")
-    public String helloworld(){
+    public String helloworld(HttpServletRequest request){
+        System.out.println(request.getRealPath("test"));
         return "hello world良劲";
     }
 
